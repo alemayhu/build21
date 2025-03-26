12326e9c90dc36a51e6c2deda7470baa6a33ffa1
Alexander Alemayhu <alexander@alemayhu.com>
2025-03-26 08:28:17 +0100

    Update SITEURL to custom domain build21.alemayhu.com

--- publishconf.py
+++ publishconf.py
@@ -9,7 +9,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://alemayhu.github.io/build21'
+SITEURL = 'https://build21.alemayhu.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
